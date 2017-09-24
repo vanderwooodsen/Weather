4833dae1e9e97f960846c6312359c6a3773b0da7
Christi Pavliukh <christi@Christis-MacBook-Air.local>
2017-09-24 15:22:04 -0700

    Added weather forecast for next week

--- weather.rb
+++ weather.rb
@@ -1,25 +1,99 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'accuweather'
+require 'rainbow'
 
-def icon(n)
-  if n > 19
-    return "🌨";
+def wind(w)
+  if w == "S"
+    return "↓"
   end
+  if w == "N"
+    return "↑"
+  end
+  if w =="W"
+    return "←"
+  end
+  if w =="E"
+    return "→"
+  end
+  if ["SW", "WS", "SWS", "SSW", "WSW"].include?(w)
+    return "↙";
+  end
+  if ["WN", "NW", "WNW", "NWN", "WWN", "NNW"].include?(w)
+    return "↖";
+  end
+  if ["NE", "EN", "ENE", "NEN", "EEN","NNE"].include?(w)
+    return "↗";
+  end
+  if ["SE","ES", "ESE", "SSE", "EES", "SES"].include?(w)
+    return "↘";
+  end
+  return w
 end
 
+def icon(n)
+  n = n.to_i
+  icons = {
+      5 => "🌞",
+      7 => "🌤",
+      12 => "🌥",
+      15 => "🌧 ☔",
+      19 => "⛈ ☔",
+  }
+
+  for k, icon in icons
+    if n < k
+      return icon
+     end
+  end
+
+  return "🌨 🌬 ❆";
+end
+
+# argv-masyv yakyj dopomagae zadavaty (zchytuvaty) z terminalu
+#.first- ze pershyj element masyvu
 city= ARGV.first
 
+#v masyv lokazii [misto, kraina] my prysvojujemo pershu nazvu mista, jake znahodymo
 location_array = Accuweather.city_search(name: city)
 location = location_array.first
-puts location.city+ "\n"+ location.state
+puts location.city + " " + location.state
 
-weather = Accuweather.get_conditions(location_id:location.id, metric: true).current
-puts "Cloud cover: #{weather.cloud_cover}"
-puts "Humidity: #{weather.humidity}"
-puts "temperature: #{weather.temperature} #{weather.weather_icon}"
-puts "Wind direction: #{weather.wind_direction}"
+# otrymaly dani z Accuweather
+response = Accuweather.get_conditions(location_id:location.id, metric: true)
+
+# berem potochnu pogodu
+weather = response.current
+
+# masyv dniv prognozy pohody/forcast-to medod zakyj povertae na nastupni dni
+weather_forecast = response.forecast
+
+puts Rainbow(" cloud cover: #{weather.cloud_cover}").magenta
+puts Rainbow(" humidity: #{weather.humidity}").red
+puts Rainbow(" temperature: #{weather.temperature} #{icon(weather.weather_icon)}").orange
+puts Rainbow(" wind direction: #{weather.wind_direction}\n\n").aqua
 
 
-weather_forecast = Accuweather.get_conditions(location_id: 'cityId:53286').forecast
-last_forecast_day = weather_forecast.last
+
+for day in weather_forecast
+  puts "#{day.day_of_week}, #{day.date}"
+  weather = day.daytime
+=begin
+  puts ""#{icon(weather.weather_icon)}"
+=end
+  if weather.wind_direction != nil
+    puts Rainbow(" high temperature: #{weather.high_temperature} c°").orange
+    puts Rainbow(" low temperature: #{weather.low_temperature} c°").magenta
+    puts Rainbow(" precipitation probability: #{weather.precipitation_probability}").aqua
+    puts Rainbow(" rain probability: #{weather.rain_probability} ☔").cyan
+    puts Rainbow(" wind direction: #{wind(weather.wind_direction)}").blue
+  else
+    puts " no wind"
+  end
+
+  puts Rainbow(" sunrise: #{day.sunrise} 🌞").indianred
+  puts Rainbow(" sunset : #{day.sunset} ⛅\n\n").orange
+end
+
+
+
